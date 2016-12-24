f166dba58593a14d86dfbddb140f002456976055
Sam Sartor <me@samsartor.com>
2016-12-23 22:23:14 -0700

    Readd primary f2 changes

---
+++ server/src/main/java/blue/made/angleserver/entity/structure/tower/DirectionalTower.java
@@ -0,0 +1,57 @@
+package blue.made.angleserver.entity.structure.tower;
+
+import blue.made.angleserver.Player;
+import blue.made.angleserver.entity.Entity;
+import blue.made.angleserver.entity.EntitySpec;
+import blue.made.angleserver.util.bounds.BoundQ;
+import blue.made.angleserver.world.World;
+import blue.made.bcf.BCFMap;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by Mobius on 12/10/16.
+ */
+public abstract class DirectionalTower extends Tower{
+    public class DirectionalTowerEntity extends Tower.TowerEntity {
+        public float angle;
+        public DirectionalTowerEntity(long uuid) {
+            super(uuid);
+        }
+
+        public void setRange(float distance, World world) {
+            int rad = (int) (distance + 0.999);
+            buildTargets(world, q -> q.box(-rad, -rad, rad, rad));
+        }
+
+        public void setAngle(float angle, World world) {
+            this.angle = angle;
+        }
+
+        @Override
+        public boolean attack(Entity e) {
+            // TODO check if in radius
+            return ((DirectionalTower) getSpec()).attack(e, this);
+        }
+
+        @Override
+        public EntitySpec<Entity> getSpec() {
+            return DirectionalTower.this;
+        }
+    }
+
+    @Override
+    public Consumer<BoundQ> getDefaultBounds() {
+        return q -> q.box(-1, -1, 1, 1);
+    }
+
+    @Override
+    public Entity requestSpawn(Player p, BCFMap data) {
+        return new DirectionalTower.DirectionalTowerEntity(newUUID());
+    }
+
+    /**
+     * Do the attack.
+     */
+    protected abstract boolean attack(Entity target, DirectionalTower.DirectionalTowerEntity attacker);
+}
---
+++ server/src/main/java/blue/made/angleserver/entity/structure/tower/Tower.java
@@ -0,0 +1,66 @@
+package blue.made.angleserver.entity.structure.tower;
+
+import blue.made.angleserver.entity.Entity;
+import blue.made.angleserver.entity.structure.StructureEntity;
+import blue.made.angleserver.entity.structure.StructureSpec;
+import blue.made.angleserver.entity.structure.req.BuildReq;
+import blue.made.angleserver.entity.structure.req.BuildReqAnd;
+import blue.made.angleserver.entity.structure.req.BuildReqOverlap;
+import blue.made.angleserver.entity.structure.req.BuildReqTilesAre;
+import blue.made.angleserver.util.bounds.BoundQ;
+import blue.made.angleserver.util.bounds.GridBoundQ;
+import blue.made.angleserver.world.Chunk;
+import blue.made.angleserver.world.World;
+import blue.made.angleserver.world.tags.Tags;
+
+import java.util.HashSet;
+import java.util.function.Consumer;
+
+/**
+ * Created by sam on 12/8/16.
+ */
+public abstract class Tower<E extends Tower.TowerEntity> extends StructureSpec<E> {
+    public static abstract class TowerEntity extends StructureEntity {
+        public Chunk[] toAttack = new Chunk[0];
+
+        public TowerEntity(long uuid) {
+            super(uuid);
+        }
+
+        @Override
+        public void tick(World world) {
+            for (Chunk c : toAttack) {
+                // TODO call attack on entities
+            }
+        }
+
+        /**
+         * Call this function to specify the bounds of the tower's attack range. This is just an
+         * optimization, further checks will be needed in {@link #attack(Entity)}.
+         * <br>
+         * MAKE SURE TO CALL THIS AT LEAST ONCE, PROBABLY IN {@link #postSpawn()}
+         */
+        public void buildTargets(World world, Consumer<BoundQ> range) {
+            HashSet<Chunk> locs = new HashSet<>();
+            BoundQ q = new GridBoundQ(World.CHUNK_WIDTH, l -> {
+                locs.add(world.getChunk(l.x, l.y));
+                return false;
+            });
+            toAttack = new Chunk[locs.size()];
+            locs.toArray(toAttack);
+        }
+
+        /**
+         * Checks if an entity is is range and attacks it
+         *
+         * @return true if the entity can be (and was) attacked
+         */
+        public abstract boolean attack(Entity e);
+    }
+
+    @Override
+    public BuildReq getBuildReq() {
+        // Tower must not overlap other structures and must be on ground
+        return new BuildReqAnd(new BuildReqOverlap(), new BuildReqTilesAre(Tags.ground));
+    }
+}
